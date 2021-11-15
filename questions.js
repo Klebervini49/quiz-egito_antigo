d2f934686c67b748ec1eac206f70159af8138714
Kleberson Vinicius <klebervini49@gmail.com>
2021-11-15 00:59:44 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+        question: 'Qual o nome do rio que banha o Egito?',
+        options: [
+            'Tigre',
+            'Au-au',
+            'Nilo',
+            'Iguaçu',
+            'Despedida'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Para que servia as pirâmides?',
+        options: [
+            'Enterrar os faraós',
+            'Guardar comida',
+            'Enfeitar o deserto',
+            'Salão de festas',
+            'Para batizar as crianças'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Para que servia o shaduf?',
+        options: [
+            'Para pescar',
+            'Para nadar',
+            'Para tirar água do rio',
+            'Tomar banho',
+            'Para fazer comida'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como era feito o papel egipicio?',
+        options: [
+            'Celulose',
+            'Papiro',
+            'Couro animal',
+            'Palha',
+            'Argila'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Por que os egípicios mumificavam os corpos?',
+        options: [
+
+            'Acreditavam na vida após a morte',
+            'Para não feder',
+            'Pois eram higiênicos',
+            'Para se purificar',
+            'Pois achavam múmias bonitas',
+        ],
+        answer: 0
+    },
+    {
+        question: 'Como era o nome do livro que enterravam junto com os mortos?',
+        options: [
+            'Bíblia',
+            'Livro dos mortos',
+            'Gibi',
+            'Corão',
+            'Vedas',
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quais os nomes das principais pirâmides?',
+        options: [
+            'Sacaré, djoser, tiries',
+            'Queops, quefrem e miquerinos',
+            'meidins, tanqueres, sefrés',
+            'Sebeferu, zauite, raturés',
+            'Unas, teti, pepi'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Como se chama o túmulo onde era colocado o faraó?',
+        options: [
+            'Duna',
+            'Amon',
+            'Sarcófago',
+            'Estojo de madeira',
+            'Caixa preta'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Nome da escrita utilizada pelos egípcios para registrar textos?',
+        options: [
+            'Hieróglifos',
+            'Desenhos',
+            'Letras',
+            'Cuneiforme',
+            'Pintura rupestre'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quem tinha o maior poder na sociedade egípcia?',
+        options: [
+            'General',
+            'Faraó',
+            'Sacerdote',
+            'Presidente',
+            'Primeiro ministro'
+        ],
+        answer: 1
+    },
+];
